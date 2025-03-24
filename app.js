f9f4bc65364bbb82c1559d09257c45ccf5c50d95
loaiattar <loaiattar@gmail.com>
2025-03-24 14:01:37 +0100

    fix: :bug: modification app.js et changer le modules dans package.js

--- app.js
+++ app.js
@@ -1,57 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Item from './routes/items.js'; 
+import itemRoutes from './routes/items.js';  
+import { connectDB } from './database/db.js';  
 
 const app = express();
 const port = 3000;
 
-
+// middleware parse JSON 
 app.use(express.json());
 
-// Connexion à MongoDB
-const dbURI = 'mongodb://localhost:27017/my_project_db'; 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB connecté avec succès');
-  })
-  .catch((err) => {
-    console.log('Erreur de connexion à MongoDB:', err);
-  });
+// Connectio DB
+connectDB();
 
-// GET /items
-app.get('/items', async (req, res) => {
-  try {
-    const items = await Item.find(); // chercher tous les items dans la collection
-    res.json(items); // retourner les items sous forme de JSON
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Items API!');
 });
 
-// POST /items - recevoir un objet et le sauvegarder dans la DB
-app.post('/items', async (req, res) => {
-  const { name, description, price } = req.body;
 
-  if (!name || !description || !price) {
-    return res.status(400).json({ message: 'Tous les champs sont requis' });
-  }
+app.use('/items', itemRoutes);
 
-  try {
-    const newItem = new Item({
-      name,
-      description,
-      price,
-    });
 
-    await newItem.save(); 
-
-    res.status(201).json(newItem); 
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Démarrer le serveur
 app.listen(port, () => {
-  console.log(`Serveur démarré sur http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
